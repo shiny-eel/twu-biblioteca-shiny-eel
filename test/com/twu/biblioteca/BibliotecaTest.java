5e477b6afe0a58c9dbdd660f3f07dfb052e41242
Lu Shien Lee <lushienlee@gmail.com>
2020-06-29 15:47:18 +1200

    Refactor welcome msg test

--- test/com/twu/biblioteca/BibliotecaTest.java
+++ test/com/twu/biblioteca/BibliotecaTest.java
@@ -1,12 +1,19 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
 import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
 
+import static org.hamcrest.core.IsCollectionContaining.hasItem;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
@@ -16,13 +23,20 @@ public class BibliotecaTest {
     public MockitoRule rule = MockitoJUnit.rule();
 
 
+    private PrintStream mockOut;
+
+    @Before
+    public void setUp() throws Exception {
+        mockOut = mock(PrintStream.class);
+        System.setOut(mockOut);
+    }
+
     @Test
     public void testWelcomeMessage() {
-        PrintStream out = mock(PrintStream.class);
-        System.setOut(out);
         BibliotecaApp.main(new String[]{});
         String expected = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
-        verify(out).println(expected);
+        verify(mockOut).println(expected);
+    }
 
     }
 }
