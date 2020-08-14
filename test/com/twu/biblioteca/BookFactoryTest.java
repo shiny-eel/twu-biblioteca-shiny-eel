73fdbf4ee7c33586126523deac7ecefe89664131
Lu Shien Lee <lushienlee@gmail.com>
2020-08-14 16:22:58 +1200

    Refactor nomenclature and add year test

--- test/com/twu/biblioteca/BookFactoryTest.java
+++ test/com/twu/biblioteca/BookFactoryTest.java
@@ -19,7 +19,7 @@ public class BookFactoryTest {
 
 
     @Test
-    public void testReadJSON() throws Exception {
+    public void testBookTitles() throws Exception {
         BookFactory reader = new BookFactory(mockApp);
         reader.createBooks();
         assertThat(reader.getBookList().get(0).title, is("Art of War"));
@@ -29,12 +29,22 @@ public class BookFactoryTest {
     }
 
     @Test
-    public void testReadJSONDeep() throws Exception {
+    public void testBookAuthors() throws Exception {
         BookFactory reader = new BookFactory(mockApp);
         reader.createBooks();
         assertThat(reader.getBookList().get(0).author, is("Sun Tzu"));
         assertThat(reader.getBookList().get(1).author, is("David Foster Wallace"));
         assertThat(reader.getBookList().get(2).author, is("Malcolm Gladwell"));
+
+    }
+
+    @Test
+    public void testBookYears() throws Exception {
+        BookFactory reader = new BookFactory(mockApp);
+        reader.createBooks();
+        assertThat(reader.getBookList().get(0).year, is(500));
+        assertThat(reader.getBookList().get(1).year, is(1996));
+        assertThat(reader.getBookList().get(2).year, is(2013));
 
     }
 }
